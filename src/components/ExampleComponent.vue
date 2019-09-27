f61e35beb42b24459c39408b77b2e5abf5e980d2
Juha Laurikainen <jmlaurikainen@gmail.com>
2019-09-27 04:27:42 +0300

    Landing slides

---
+++ src/components/ExampleComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="hello">This is the HelloWorld component</div>
+</template>
+
+<script>
+export default {
+  name: "ExampleComponent",
+  props: {}
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-content>
+    <div class="first-time-input">
+      <v-row justify="center">
+        <v-col md="auto">
+          <v-text-field
+            label="Your weight"
+            type="number"
+            prepend-icon="mdi-weight-kilogram"
+            id="user-weight"
+            outlined
+            suffix="kg"
+            :rules="[rules.required, rules.min, rules.max]"
+          ></v-text-field>
+          <div class="text-center">
+            <v-btn>
+              Next
+              <v-icon right>mdi-chevron-right</v-icon>
+            </v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </div>
+  </v-content>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import ExampleComponent from '../components/ExampleComponent';
 
 export default {
-  name: "home",
   components: {
-    HelloWorld
+    //ExampleComponent,
+  },
+  data() {
+    return {
+      rules: {
+        required: value => !!value || "Required.",
+        min: value => parseFloat(value) >= 10 || "Weight too low.",
+        max: value => parseFloat(value) <= 635 || "Weight too high."
+      }
+    };
   }
 };
 </script>
+
+<style scoped></style>
