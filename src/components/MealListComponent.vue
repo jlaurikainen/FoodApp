0b568120b11d5bd8ac6bd052029ab696ebcbe34e
Juha Laurikainen <jmlaurikainen@gmail.com>
2019-11-19 20:48:36 +0200

    Meal list to component, using props

---
+++ src/components/MealListComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="row">
+    <div class="column u-mb-2-5">
+      <table class="u-full-width u-mb-2-5">
+        <tbody>
+          <tr v-for="meal in meals" :key="meal.id">
+            <td>{{ meal.name }}</td>
+            <td>{{ meal.calories }} kcal</td>
+          </tr>
+        </tbody>
+      </table>
+      <router-link to="/addmeal" tag="button" class="u-pull-right button-primary">
+        <i class="material-icons u-mr-1">add</i>Lisää ateria
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["meals"]
+}
+</script>
+
+<style scoped>
+button {
+  margin-bottom: 0px;
+  padding: 0px 15px;
+  display: flex;
+}
+
+.material-icons {
+  font-size: 1.5rem;
+  align-self: center;
+}
+
+td:last-child {
+  text-align: right;
+}
+
+td:only-child {
+  text-align: left;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,26 +7,19 @@
       </ul>
       <router-view />
     </div>
-    <div class="column u-mb-2-5">
-      <table class="u-full-width u-mb-2-5">
-        <tbody>
-          <tr v-for="meal in meals" :key="meal.id">
-            <td>{{ meal.name }}</td>
-            <td>{{ meal.calories }} kcal</td>
-          </tr>
-        </tbody>
-      </table>
-      <router-link to="/addmeal" tag="button" class="u-pull-right button-primary">
-        <i class="material-icons u-mr-1">add</i>Lisää ateria
-      </router-link>
-    </div>
+    <MealListComponent :meals="meals" />
   </div>
 </template>
 
 <script>
+import MealListComponent from "../components/MealListComponent";
+
 import { mapState } from "vuex";
 
 export default {
+  components: {
+    MealListComponent
+  },
   computed: {
     ...mapState({
       meals: state => state.meals
@@ -36,22 +29,5 @@ export default {
 </script>
 
 <style scoped>
-button {
-  margin-bottom: 0px;
-  padding: 0px 15px;
-  display: flex;
-}
 
-.material-icons {
-  font-size: 1.5rem;
-  align-self: center;
-}
-
-td:last-child {
-  text-align: right;
-}
-
-td:only-child {
-  text-align: left;
-}
 </style>
